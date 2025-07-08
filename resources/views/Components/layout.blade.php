39f35a8617488797d64a936ba56fa1cd2a171a3c
SHIVA NAIDU <dasarisambasivanaidu7@gmail.com>
2025-07-08 11:30:09 +0530

    chore: update project dependencies and configuration; rename app and adjust Vite setup for CSS integration

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $heading }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+    @vite('resources/js/app.js')
+</head> 
 <body class="h-full">
 
 <div class="min-h-full">
